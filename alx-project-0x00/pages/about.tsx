739fada6a2ab7673ca5fe5065f6da20316c466d4
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-12-20 22:42:11 +0300

    Basic Routing

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,10 @@
+const About: React.Fc = () => {
+    return (
+        <div>
+            <h1 className="text-xl font-extralight">
+                About Page
+            </h1>
+        </div>
+    )
+}
+export default About;
---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,11 @@
+const Landing: React.Fc = () => {
+    return (
+        <div>
+            <h1 className="text-xl font-extralight">
+                Landing Page
+            </h1>
+        </div>
+    )
+}
+
+export default Landing;
