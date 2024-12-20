8e29f35a9da55a466d7cf5ed2f118e9a546d5139
natnael-shiferaw <natnaelshiferaw2123@gmail.com>
2024-12-20 23:21:34 +0300

    Using components

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,12 @@
+import Card from "@/components/Card";
+
 const Landing: React.Fc = () => {
     return (
         <div>
             <h1 className="text-xl font-extralight">
                 Landing Page
             </h1>
+            <Card />
         </div>
     )
 }
